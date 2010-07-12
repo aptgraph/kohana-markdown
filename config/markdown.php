e60c3298e4202d635e4bc2bda7602c50c372c1f8
Gary Stidston-Broadbent <gary@stroppytux.net>
2010-07-13 00:09:28 +0800

    Added initial version of the markdown module.

---
+++ config/markdown.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+return array
+(
+	'default'   => array
+	(
+		'type'				=> 'xhtml',		// html or xhtml
+		'tab_width'			=> 4,			// Tab width for output
+		// Many more options to come
+	)
+);
+?>
